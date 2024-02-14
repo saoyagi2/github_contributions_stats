b90dc6edfa79d7218c1d75054bca33b83b78e3df
Shigekazu Aoyagi <saoyagi2@gmail.com>
2024-02-14 09:47:09 +0900

    GitHub API の実際の呼び出し

--- app.rb
+++ app.rb
@@ -1,16 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/config_file'
 
 require_relative 'lib/contribution_manager'
 require_relative 'lib/github_api'
  
 set :show_exceptions, false
 
+config_file './config.yml'
+
 get '/' do
   @username = params[:username]
 
   unless @username.nil? || @username.empty?
-    github_api = GithubApi.new(@username)
+    github_api = GithubApi.new(settings.github, @username)
 
     @years = github_api.get_years
 
